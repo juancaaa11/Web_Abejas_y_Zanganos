2d4ba4eec68315b79a46d13dc531bc6de0ca7c6d
juancaaa11 <juanca.jcglp26@gmail.com>
2024-11-12 00:22:57 +0100

    v2.5

--- _script/script.js
+++ _script/script.js
@@ -13,9 +13,26 @@
      });
  }
 
- function irASeccion5() {
+ function irASeccion4() {
     document.getElementById("seccion4").scrollIntoView({ behavior: 'smooth' });
   }
+
+  function irASeccion2() {
+    document.getElementById("seccion2").scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function irASeccion6() {
+    document.getElementById("seccion6").scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function irASeccion5() {
+    document.getElementById("seccion5").scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function irASeccion3() {
+    document.getElementById("seccion3").scrollIntoView({ behavior: 'smooth' });
+  }
+
 
  // Ejecutar cuando se haga scroll
  window.addEventListener('scroll', verificarVisibilidad);
